60d1a809d3cff5e86e3e383bd6309c87ae7ff3a8
Sayalijunawane <sayali.junawane@waiin.com>
2022-08-02 14:17:51 +0530

    commit in search

--- src/test/java/pageObject/Search.java
+++ src/test/java/pageObject/Search.java
@@ -192,7 +192,7 @@ public class Search extends base
 		String MonthyearValueFrom;
 		Surgeries s=new Surgeries();
 		s.FilterbyDate(driver).click();
-		
+		//if today selected 
 		driver.findElement(By.cssSelector(".available.ng-star-inserted.today.active.end-date.start-date")).click();
 		System.out.println("today date selected");
 		/*MonthyearValueFrom=driver.findElement(By.xpath("//th[@class='month drp-animate']")).getText();
